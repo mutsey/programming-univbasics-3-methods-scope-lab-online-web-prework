49e790c68565f4f13bd81df94075274b360b6fd0
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-04 21:08:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,3 +6,8 @@ end
 def toadstool
   puts status
 end
+describe "any_phrase" do
+  it "It's Dangeroud To Go Alone! Take This." do
+    phrase = "Do A Barrel Roll!"
+    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+  end
