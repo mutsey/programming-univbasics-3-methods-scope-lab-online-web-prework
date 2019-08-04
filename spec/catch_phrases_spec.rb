adc46814b2fea6bd257e62dfe203135ab89cc940
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-04 21:18:40 +0000

    Automatically backed up by Learn

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -1,4 +1,4 @@
-describe "any_phrase" do
+describe "" do
   it "takes in an argument and puts out the catch phrase" do
     phrase = "Do A Barrel Roll!"
     expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
