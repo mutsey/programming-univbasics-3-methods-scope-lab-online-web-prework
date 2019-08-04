c7eb9a43c04ba6c9c36960f94c1f38b072fc794c
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-04 21:10:37 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,7 +2,6 @@ def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
-
 describe "link" do
   it "puts out 'It's Dangerous To Go Alone! Take This.'" do
     expect{link}.to output("It's Dangerous To Go Alone! Take This.\n").to_stdout
