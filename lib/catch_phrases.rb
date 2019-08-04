a12258c1b710f7d890ade5633f3931b93b1de738
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-04 21:09:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,11 +3,3 @@ def mario
   puts phrase
 end
 
-def toadstool
-  puts status
-end
-describe "any_phrase" do
-  it "It's Dangeroud To Go Alone! Take This." do
-    phrase = "Do A Barrel Roll!"
-    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
-  end
